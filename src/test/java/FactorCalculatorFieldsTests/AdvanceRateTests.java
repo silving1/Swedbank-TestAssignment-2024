16a08443ff21649cc97831d4d864395d66e91c19
silve <siingv@taltech.ee>
2024-04-03 18:47:57 +0300

    AdvanceRateTests setup, first tests

--- src/test/java/FactorCalculatorFieldsTests/AdvanceRateTests.java
+++ src/test/java/FactorCalculatorFieldsTests/AdvanceRateTests.java
@@ -1,2 +1,46 @@
-public class AdvanceRateTests {
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class AdvanceRateTests extends BaseFieldsTest {
+
+    SelenideElement id = $(By.id("D6"));
+
+
+    //Test case to verify if correct values are selectable from the dropdown menu
+    @Test
+    public void isCorrectAdvanceRateValues() {
+        id.selectOption("75"); id.shouldHave(text("75"));
+        id.selectOption("80"); id.shouldHave(text("80"));
+        id.selectOption("85"); id.shouldHave(text("85"));
+        id.selectOption("90"); id.shouldHave(text("90"));
+    }
+
+    //Test case to verify if a random value is selectable from the dropdown menu
+    @Test
+    public void isFalseAdvanceRateValues() {
+        //List together all elements
+        ElementsCollection elementList = id.getSelectedOptions();
+
+        //Check, if a false value is in list using helper method below
+        assertFalse(doesValueExist(elementList, "100"));
+        assertFalse(doesValueExist(elementList, "0"));
+        assertFalse(doesValueExist(elementList, ""));
+        assertFalse(doesValueExist(elementList, "abc"));
+    }
+
+    //Helper method to loop each checkable value through the list
+    private boolean doesValueExist(ElementsCollection elementList, String valueToCheck) {
+        for (SelenideElement element : elementList) {
+            if (element.equals(valueToCheck)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
