5b9e180c8ec094ad49c0bb3628d76c62ba65dbf1
silve <siingv@taltech.ee>
2024-04-03 14:39:08 +0300

    added setup

--- src/test/java/FactorCalculatorTests.java
+++ src/test/java/FactorCalculatorTests.java
@@ -1,8 +1,12 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -11,23 +15,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorCalculatorTests {
 
+    @BeforeEach
+    public void setUp() {
+        Configuration.browser = "chrome";
+        open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
+        WebDriverRunner.getWebDriver().manage().window().maximize();
+        if($("button.ui-cookie-consent__accept-button").is(visible)){
+            $(byText("Accept")).click();
+        }
+
+    }
+    @AfterEach
+    public void tearDown() {
+        closeWebDriver();
+    }
+
     @Test
     public void isFactoringCalculatorUrlExists() {
-        open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
         String url = WebDriverRunner.url();
         assertEquals(url, "https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
     }
     @Test
     public void isFactoringCalculatorExists() {
-        open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
         $("h1").shouldHave(text("Factoring"));
         $("ui-calculator").shouldBe(visible);
     }
     @Test
     public void isFactoringCalculatorFieldsExists() {
-        open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
-        $(byText("Accept")).click();
-
         $("#D5").should(exist); $("#D5").shouldBe(visible);
         $("#D7").should(exist); $("#D7").shouldBe(visible);
         $("#D9").should(exist); $("#D9").shouldBe(visible);
@@ -37,9 +51,6 @@ public class FactorCalculatorTests {
     }
     @Test
     public void canAddValuesToFactoringCalculatorFields() {
-        open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
-        $(byText("Accept")).click();
-
         $("#D5").val("9000");
         $("#D7").val("15");
         $("#D9").val("1");
@@ -49,9 +60,6 @@ public class FactorCalculatorTests {
     }
     @Test
     public void isFactoringCalculatorCalculateButtonExists() {
-        open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
-        $(byText("Accept")).click();
-
         $("#calculate-factoring").should(exist); $("#calculate-factoring").shouldBe(visible);
         $("#result_perc").should(exist); $("#result_perc").shouldBe(visible);
         $("#result_perc").shouldHave(text("0"));
@@ -60,9 +68,6 @@ public class FactorCalculatorTests {
     }
     @Test
     public void doesFactoringCalculatorCalculateButtonCalculate() {
-        open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
-        $(byText("Accept")).click();
-
         $(By.id("calculate-factoring")).click();
         $("#result_perc").shouldHave(text("0.53"));
         $("#result").shouldHave(text("52.50"));
