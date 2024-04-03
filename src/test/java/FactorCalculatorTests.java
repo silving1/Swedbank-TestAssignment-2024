a155f2937861b18477dce71417331f6b94fc3e07
silve <siingv@taltech.ee>
2024-04-03 13:37:38 +0300

    test isFactoringCalculatorFieldsExists

--- src/test/java/FactorCalculatorTests.java
+++ src/test/java/FactorCalculatorTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,9 +19,19 @@ public class FactorCalculatorTests {
     @Test
     public void isFactoringCalculatorExists() {
         open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
-        //$(byText("Accept")).click();
-
-        $("ui-calculator").shouldBe(visible);
         $("h1").shouldHave(text("Factoring"));
+        $("ui-calculator").shouldBe(visible);
+    }
+    @Test
+    public void isFactoringCalculatorFieldsExists() {
+        open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
+        $(byText("Accept")).click();
+
+        $("#D5").should(exist); $("#D5").shouldBe(visible);
+        $("#D7").should(exist); $("#D7").shouldBe(visible);
+        $("#D9").should(exist); $("#D9").shouldBe(visible);
+
+        $("#D6").should(exist); $("#D6").shouldBe(visible);
+        $("#D8").should(exist); $("#D8").shouldBe(visible);
     }
 }
