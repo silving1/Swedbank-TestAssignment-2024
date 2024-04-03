b8773ef789b310ee5095c828071801a3ed6e439e
silve <siingv@taltech.ee>
2024-04-03 13:22:52 +0300

    fixed isFactoringCalculatorExists

--- src/test/java/FactorCalculatorTests.java
+++ src/test/java/FactorCalculatorTests.java
@@ -7,7 +7,6 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class FactorCalculatorTests {
 
     @Test
@@ -20,7 +19,8 @@ public class FactorCalculatorTests {
     public void isFactoringCalculatorExists() {
         open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
         //$(byText("Accept")).click();
+
         $("ui-calculator").shouldBe(visible);
-        $("Factoring").shouldBe(visible);
+        $("h1").shouldHave(text("Factoring"));
     }
 }
