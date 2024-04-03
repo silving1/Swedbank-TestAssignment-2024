c12da621a905f610e5df53a7a2c9cacab1942e85
silve <siingv@taltech.ee>
2024-04-03 16:57:20 +0300

    Added documentation

--- src/test/java/FactorCalculatorFieldsTests/InvoiceAmountTests.java
+++ src/test/java/FactorCalculatorFieldsTests/InvoiceAmountTests.java
@@ -36,6 +36,8 @@ public class InvoiceAmountTests {
     //Different test scenarios, where user tries to input false values to Invoice Amount field
     @Test
     public void falseInvoiceAmountValues() {
+        //Made a private helper method below to help me repeat each test.
+        //Firstly inputs value the website doesn't like, then it should return error corresponding message
         testValueInput("0", "Value must be greater than or equal 1.");
         testValueInput("-5000", "Value must be greater than or equal 1.");
         testValueInput("abc", "Please fill out this field.");
@@ -48,6 +50,7 @@ public class InvoiceAmountTests {
     //Different test scenarios, where user tries to input unusual, but true values to Invoice Amount field
     @Test
     public void trueInvoiceAmountValues() {
+        //Firstly inputs value the website likes, there should be no error messages
         testValueInput("1", "");
         testValueInput("1000000000000000000000000000", "");
         testValueInput("0000000000000001", "");
@@ -58,6 +61,7 @@ public class InvoiceAmountTests {
     //Test case, where user inputs true and false value after another
     @Test
     public void trueAndFalseInvoiceAmountValues() {
+        //Tests, if the error message disappears after correct input and comes back, when inserted false input
         testValueInput("0", "Value must be greater than or equal 1.");
         testValueInput("1000", "");
         testValueInput("10+10", "Please enter a valid value.");
@@ -68,10 +72,14 @@ public class InvoiceAmountTests {
     // Test case, where user inputs false value and clicks Calculate button
     @Test
     public void falseInvoiceAmountValueAndCalculate() {
+        //Firstly checks, if initial calculation is done
         errorMsg.shouldNot(exist);
         FCTests.doesFactoringCalculatorCalculateButtonCalculate();
+
+        //Then adding user input the website doesn't like
         testValueInput("0", "Value must be greater than or equal 1.");
 
+        //After false input, the result should show 0
         $(By.id("calculate-factoring")).click();
         $("#result_perc").shouldHave(text("0"));
         $("#result").shouldHave(text("0"));
@@ -80,23 +88,32 @@ public class InvoiceAmountTests {
     // Test case, where user inputs true value and clicks Calculate button
     @Test
     public void trueInvoiceAmountValueAndCalculate() {
+        //Firstly checks, if initial calculation is done
         errorMsg.shouldNot(exist);
         FCTests.doesFactoringCalculatorCalculateButtonCalculate();
+
+        //Then adding user input the website likes
         testValueInput("10000000000000000000000000000", "");
 
+        //After correct input, the result should show the made calculation
         $(By.id("calculate-factoring")).click();
         $("#result_perc").shouldHave(text("0.53"));
         $("#result").shouldHave(text("5.25e+25"));
     }
 
-    // Method to test a value in the Invoice Amount field and validate the error message
+    // Helper method to test a value in the Invoice Amount field and validate the error message
     private void testValueInput(String value, String expectedErrorMessage) {
+        //Inserts the user value to field
         $("#D5").val(value);
+
+        //Checks, if the expected is empty
         if (!expectedErrorMessage.isEmpty()) {
+            //The expected error message is not empty. The system confirms, if the expectation is correct
             errorMsg.should(exist);
             errorMsg.shouldBe(visible);
             errorMsg.shouldHave(text(expectedErrorMessage));
         } else {
+            //The expected error message is empty. The system confirms, if the expectation is correct
             errorMsg.shouldNot(exist);
         }
     }
