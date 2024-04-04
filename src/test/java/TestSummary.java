ba85854bba9b24348a70d3084cfd7241151a52ab
silve <siingv@taltech.ee>
2024-04-04 11:54:00 +0300

    test summary done

--- src/test/java/TestSummary.java
+++ src/test/java/TestSummary.java
@@ -1,20 +1,44 @@
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
 
 import java.util.HashMap;
 import java.util.Map;
 
 //Im aware that I can find the test summary under the test folder in my files, but I am not sure
 //thats what is needed when the task says "Create test summary". So I made this.
-public class TestSummary {
-    private Map<String, String> testResults = new HashMap<>();
+public class TestSummary implements TestWatcher {
+    private Map<String, Boolean> testResults = new HashMap<>();
 
-    protected void testFailed(ExtensionContext context, Throwable cause) {
-        String testName = context.getDisplayName();
-        testResults.put(testName, "Failed");
+    @Override
+    public void testFailed(ExtensionContext context, Throwable cause) {
+        testResults.put(context.getDisplayName(), false);
     }
 
-    protected void testSuccessful(ExtensionContext context) {
-        String testName = context.getDisplayName();
-        testResults.put(testName, "Passed");
+    @Override
+    public void testSuccessful(ExtensionContext context) {
+        testResults.put(context.getDisplayName(), true);
+    }
+
+    public void printTestSummary() {
+        int totalTests = testResults.size();
+        int passedTests = 0;
+        int failedTests = 0;
+
+        System.out.println("Test Summary:");
+        for (Map.Entry<String, Boolean> entry : testResults.entrySet()) {
+            String testName = entry.getKey();
+            boolean testResult = entry.getValue();
+            if (testResult) {
+                passedTests++;
+                System.out.println("  - " + testName + ": Passed");
+            } else {
+                failedTests++;
+                System.out.println("  - " + testName + ": Failed");
+            }
+        }
+
+        System.out.println("\nTotal Tests: " + totalTests);
+        System.out.println("Passed Tests: " + passedTests);
+        System.out.println("Failed Tests: " + failedTests);
     }
 }
