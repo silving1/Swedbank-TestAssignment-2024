d65f47a4a14cdcba647b8420aad45fdc9ffaefe7
silve <siingv@taltech.ee>
2024-04-04 11:24:37 +0300

    test summary setup

--- src/test/java/TestSummary.java
+++ src/test/java/TestSummary.java
@@ -1,7 +1,20 @@
+import org.junit.jupiter.api.extension.ExtensionContext;
 
+import java.util.HashMap;
+import java.util.Map;
 
 //Im aware that I can find the test summary under the test folder in my files, but I am not sure
 //thats what is needed when the task says "Create test summary". So I made this.
 public class TestSummary {
+    private Map<String, String> testResults = new HashMap<>();
 
+    protected void testFailed(ExtensionContext context, Throwable cause) {
+        String testName = context.getDisplayName();
+        testResults.put(testName, "Failed");
+    }
+
+    protected void testSuccessful(ExtensionContext context) {
+        String testName = context.getDisplayName();
+        testResults.put(testName, "Passed");
+    }
 }
