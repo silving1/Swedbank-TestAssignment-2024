a0a051bb6371c3d98e0d3f25dc43f22c2c3abbcb
silve <siingv@taltech.ee>
2024-04-04 11:18:51 +0300

    new class for test summary

---
+++ src/test/java/TestSummary.java
@@ -0,0 +1,7 @@
+
+
+//Im aware that I can find the test summary under the test folder in my files, but I am not sure
+//thats what is needed when the task says "Create test summary". So I made this.
+public class TestSummary {
+
+}
